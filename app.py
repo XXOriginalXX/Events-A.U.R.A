0a28e457ef937dfdf9a8c1ef4a92a2ebcbe0e6f9
Adithyan S Pillai <101512399+XXOriginalXX@users.noreply.github.com>
2025-03-13 01:22:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 import uuid
+import json
 
 # Access credentials from secrets
 credentials_info = st.secrets["GOOGLE_CREDENTIALS"]
 
 # Google Sheets Authentication
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_info(eval(credentials_info), scopes=scope)
+creds = Credentials.from_service_account_info(json.loads(credentials_info), scopes=scope)
 client = gspread.authorize(creds)
 sheet = client.open("Events").sheet1
 
